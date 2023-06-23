bdd2dfe12ccd86494eb4630742b03819c45ea3a4
cyrus fullam <cyrusfullam@gmail.com>
2023-06-22 22:05:35 -0400

    added update and delete

--- controllers/routes.js
+++ controllers/routes.js
@@ -21,7 +21,9 @@ router.post("/create", (req, res) => {
         beerDB.push(newEntry);
         // write new changes to the .json file
         save(beerDB, dbPath);
-        res.status(200).json
+        res.status(200).json({
+            message: "Beer added"
+        })
     } catch(err){
         res.status(500).json({
             message: `${err}`
@@ -46,7 +48,7 @@ router.get("/allbeers", (req, res) => {
 // TODO: GET api/:id --> get one beer
 router.get("/:id", (req, res) => {
     try{
-           // destructure the id value from the request
+        // destructure the id value from the request
         const { id } = req.params;
         // get your json file contents
         const db = read(dbPath);
@@ -64,6 +66,56 @@ router.get("/:id", (req, res) => {
 
 })
 // TODO: PUT api/update/:id --> edit one beer
+router.put("/:id", (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        const db = read(dbPath);
+        
+        const foundBeer = db.find(beer => beer.id === id);
+        
+        if (!foundBeer) throw Error("Beer not found")
+        
+        let update = { id, ...req.body };
+        let index = db.indexOf(foundBeer);
+        db.splice(index, 1, update);
+
+        save(db, dbPath);
+
+        res.status(200).json({
+            message: "Beer updated"
+        })
+    } catch(err) {
+        res.status(500).json({
+            message: `${err}`
+        })
+    }
+})
 // TODO: DELETE api/delete/:id --> delete one beer
+
+router.delete("/:id", (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        const db = read(dbPath);
+        
+        const foundBeer = db.find(beer => beer.id === id);
+        
+        if (!foundBeer) throw Error("Beer not found")
+        
+        let removed = db.filter(beer => beer !== foundBeer);
+
+        save(removed, dbPath);
+
+        res.status(200).json({
+            message: "Beer deleted"
+        })
+    } catch(err) {
+        res.status(500).json({
+            message: `${err}`
+        })
+    }
+})
+
 
 module.exports = router;
